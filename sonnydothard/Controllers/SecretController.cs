e4da9d0c40f79690aea545b63d0e2ea306121ee8
Sonny D <s.dothard@gmail.com>
2016-04-12 18:26:22 -0500

    secret

---
+++ sonnydothard/Controllers/SecretController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sonnydothard.Controllers
+{
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
